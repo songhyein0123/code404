65dda286d43f3578c63cca40a786c871a8ff25f7
Jo <johj703@nate.com>
2024-10-11 16:47:07 +0900

    fix: searchTerm state 생성 후 input에 적용

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+
 export function Search() {
+    const [searchTerm, setSearchTerm] = useState("");
     return (
         <div>
             <input
                 type="text"
-                value={}
-                onChange={}
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
                 placeholder="검색어를 입력하세요."
                 className="border rounded-1 px-2 py-1"
             />
