24d66ca50678017c415fb96820409c4bf78deaa2
Jo <johj703@nate.com>
2024-10-11 20:09:39 +0900

    fix: 게시판 이름 수정

--- src/app/main_board/page.tsx
+++ src/app/main_board/page.tsx
@@ -3,7 +3,7 @@ import Board from "@/components/Board";
 export default function main_board() {
     return (
         <div>
-            <h1 className="text-2xl font-bold text-center my-8">게시판</h1>
+            <h1 className="text-2xl font-bold text-center my-8">Main Page</h1>
             <Board />
         </div>
     );
