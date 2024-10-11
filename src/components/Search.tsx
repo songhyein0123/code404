d75756958cbb56fe0f9a029313beb7bc5f6b9ab5
Jo <johj703@nate.com>
2024-10-11 16:45:47 +0900

    fix: Search 컴포넌트 JSX 완성

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,14 @@
-const Search = () => {
-    return <div>Search</div>;
-};
-
-export default Search;
+export function Search() {
+    return (
+        <div>
+            <input
+                type="text"
+                value={}
+                onChange={}
+                placeholder="검색어를 입력하세요."
+                className="border rounded-1 px-2 py-1"
+            />
+            <button className="bg-blue-500 text-white px-4 py-1 rounded-r">검색</button>
+        </div>
+    );
+}
