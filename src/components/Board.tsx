42f9e8c47d44aa55677876f67283db54b5c4b7c9
Jo <johj703@nate.com>
2024-10-11 17:51:54 +0900

    fix: mockPosts와 interface Post 생성

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,30 @@
 import { useState } from "react";
 
+interface Post {
+    id: number;
+    title: string;
+    author: string;
+    date: string;
+    hashtags: string[];
+}
+
+const mockPosts: Post[] = [
+    {
+        id: 1,
+        title: "첫 번째 게시글",
+        author: "작성자1",
+        date: "2024-10-11",
+        hashtags: ["#TypeScript", "#Nextjs", "#React"]
+    },
+    {
+        id: 2,
+        title: "두 번째 게시글",
+        author: "작성자2",
+        date: "2024-10-11",
+        hashtags: ["#JavaScript", "#TailwindCSS", "#Nextjs"]
+    }
+];
+
 export default function Board() {
     const [currentPage, setCurrentPage] = useState(1);
     return (
