f87d5b00dc9b48193bbb59db5ad7484bc0db3f1f
Jo <johj703@nate.com>
2024-10-11 19:50:46 +0900

    fix: main_board 페이지 제목 추가

--- src/app/main_board/page.tsx
+++ src/app/main_board/page.tsx
@@ -1,5 +1,10 @@
-const main_board = () => {
-    return <></>;
-};
+import Board from "@/components/Board";
 
-export default main_board;
+export default function main_board() {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold text-center my-8">게시판</h1>
+            <Board />
+        </div>
+    );
+}
