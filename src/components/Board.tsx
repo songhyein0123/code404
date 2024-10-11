3a70630925c773d7934227e874503f1b3fc23085
Jo <johj703@nate.com>
2024-10-11 17:46:43 +0900

    fix: 게시글 목록 작성

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,5 +1,15 @@
-const Board = () => {
-    return <div>Board</div>;
-};
-
-export default Board;
+export default function Board() {
+    return (
+        <div>
+            {/* 게시글 목록 */}
+            {pagenatedPosts.map((post) => (
+                <div key={post.id}>
+                    <div>{post.title}</div>
+                    <div>
+                        <span>{post.author}</span> | <span>{post.date}</span> | <span key={idx}>{tag}</span>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
