ca87668b1a2046ad41c4485ab40c43cc20ddd559
Jo <johj703@nate.com>
2024-10-11 19:56:49 +0900

    style: Search 컴포넌트 CSS추가

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -13,13 +13,13 @@ export function Search({ onSearch }: SearchProps) {
         onSearch(searchTerm);
     };
     return (
-        <div>
+        <div className="flex items-center mb-4">
             <input
                 type="text"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 placeholder="검색어를 입력하세요."
-                className="border rounded-1 px-2 py-1"
+                className="w-full p-2 border border-gray-300 rounded-1-md focus:outline-none"
             />
             <button onClick={handleSearch} className="bg-blue-500 text-white px-4 py-1 rounded-r-md">
                 검색
