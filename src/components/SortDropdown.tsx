a6b5439e739e197e7b586b6caac380c777864755
Jo <johj703@nate.com>
2024-10-11 21:08:04 +0900

    fix: 좋아요와 게시글 정렬을 위한 interface와 sortOptions 배열 생성 및 sortDropdown함수의 인자로 onSortChange 넣기

--- src/components/SortDropdown.tsx
+++ src/components/SortDropdown.tsx
@@ -1,3 +1,17 @@
-export default function SortDropdown() {
+interface SortOption {
+    label: string;
+    value: string;
+}
+
+const sortOptions: SortOption[] = [
+    { label: "최신순", value: "latest" },
+    { label: "좋아요 순", value: "likes" }
+];
+
+interface SortDropdownProps {
+    onSortChange: (value: string) => void;
+}
+
+export default function SortDropdown({ onSortChange }: SortDropdownProps) {
     return <div>SortDropdown</div>;
 }
