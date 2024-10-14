7dc72070288f698f222cf53f933f3b2505c18c24
Jo <johj703@nate.com>
2024-10-14 18:23:00 +0900

    fix: 입력한 해시태그가 보여지는 부분, 해시태그 입력 필드 추가

--- src/app/main_board/new/page.tsx
+++ src/app/main_board/new/page.tsx
@@ -4,12 +4,39 @@ import { useState } from "react";
 
 export default function NewPostPage() {
     const [title, setTitle] = useState("");
+    const [hashtags, setHashtags] = useState<string[]>([]);
+    const [currentTag, setCurrentTag] = useState("");
     return (
         <div>
             {/* 제목 입력 */}
             <div>
                 <label>제목</label>
                 <input type="text" value={title} onChange={} className="" placeholder="글 제목을 입력하세요" />
+            </div>
+            {/* 해시태그 입력 */}
+            <div>
+                <label>해시태그</label>
+                <div>
+                    {/* 해시태그 배지 */}
+                    {hashtags.map((tag) => {
+                        <div key={tag}>
+                            {tag}
+                            <button type="button" onClick={() => removeTag(tag)} className="">
+                                &times;
+                            </button>
+                        </div>;
+                    })}
+
+                    {/* 해시태그 입력 필드 */}
+                    <input
+                        type="text"
+                        value={currentTag}
+                        onChange={handleTagInputChange}
+                        onKeyPress={handleTagKeyPress}
+                        className=""
+                        placeholder="해시태그를 입력하고 엔터를 누르세요!"
+                    />
+                </div>
             </div>
         </div>
     );
