547ea7bbc6bfeecf3e01d92f5b9ffbddb4911fce
Jo <johj703@nate.com>
2024-10-14 17:55:15 +0900

    fix: title state 생성하고 제목을 입력하는 부분 작성

--- src/app/main_board/new/page.tsx
+++ src/app/main_board/new/page.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import { useState } from "react";
+
 export default function NewPostPage() {
-    return <div>page</div>;
+    const [title, setTitle] = useState("");
+    return (
+        <div>
+            {/* 제목 입력 */}
+            <div>
+                <label>제목</label>
+                <input type="text" value={title} onChange={} className="" placeholder="글 제목을 입력하세요" />
+            </div>
+        </div>
+    );
 }
