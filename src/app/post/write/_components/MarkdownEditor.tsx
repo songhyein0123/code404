95e48cb075a127708715ab4e5facf596b2a8fe73
Jo <johj703@nate.com>
2024-10-15 23:44:58 +0900

    fix: MarkdownEditorProps interface 추가 후 onEditorChange의 타입으로 넣기

--- src/app/post/write/_components/MarkdownEditor.tsx
+++ src/app/post/write/_components/MarkdownEditor.tsx
@@ -5,10 +5,15 @@ import React, { useRef } from "react";
 import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor as ToastEditor } from "@toast-ui/react-editor";
 
+// 인터페이스 정의
+interface MarkdownEditorProps {
+    inEditorChange: (content: string) => void;
+}
+
 // ToastUI Editor를 동적으로 불러오기
 const Editor = dynamic(() => import("@toast-ui/react-editor").then((mod) => mod.Editor), { ssr: false });
 
-const MarkdownEditor = ({ onEditorChange }: { onEditorChange: (content: string) => void }) => {
+const MarkdownEditor = ({ onEditorChange }: MarkdownEditorProps) => {
     // 'useRef'에서 'ToastEditor' 타입을 사용
     const editorRef = useRef<ToastEditor>(null);
 
