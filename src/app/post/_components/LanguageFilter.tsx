f4c3f3394091b3295bf16b8afd1ba70ee29416f9
Jo <johj703@nate.com>
2024-10-16 17:40:25 +0900

    fix: 언어 선택하는 부분 코드 수정

--- src/app/post/_components/LanguageFilter.tsx
+++ src/app/post/_components/LanguageFilter.tsx
@@ -1,27 +1,25 @@
-interface LanguageFilterProps {
+export function LanguageFilter({
+    allHashtags,
+    selectedTags,
+    onTagChange
+}: {
     allHashtags: string[];
     selectedTags: string[];
     onTagChange: (tag: string) => void;
-}
-
-const LanguageFilter = ({ allHashtags, selectedTags, onTagChange }: LanguageFilterProps) => {
+}) {
     return (
         <div className="relative">
             {allHashtags.map((tag) => (
-                <div key={tag} className="px-4 py-2">
-                    <label>
-                        <input
-                            type="checkbox"
-                            checked={selectedTags.includes(tag)}
-                            onChange={() => onTagChange(tag)}
-                            className="mr2"
-                        />
-                        <span className="text-gray-700">{tag}</span>
-                    </label>
-                </div>
+                <label key={tag} className="px-4 py-2">
+                    <input
+                        type="checkbox"
+                        checked={selectedTags.includes(tag)}
+                        onChange={() => onTagChange(tag)}
+                        className="mr-2"
+                    />
+                    <span className="text-gray-700">{tag}</span>
+                </label>
             ))}
         </div>
     );
-};
-
-export default LanguageFilter;
+}
