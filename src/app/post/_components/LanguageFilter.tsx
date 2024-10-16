7f90d1da99292a78698f471f10eae26e16957244
Jo <johj703@nate.com>
2024-10-16 17:38:02 +0900

    style: 언어 선택을 보여주는 컴포넌트에 CSS 추가

--- src/app/post/_components/LanguageFilter.tsx
+++ src/app/post/_components/LanguageFilter.tsx
@@ -6,12 +6,17 @@ interface LanguageFilterProps {
 
 const LanguageFilter = ({ allHashtags, selectedTags, onTagChange }: LanguageFilterProps) => {
     return (
-        <div>
+        <div className="relative">
             {allHashtags.map((tag) => (
-                <div key={tag}>
+                <div key={tag} className="px-4 py-2">
                     <label>
-                        <input type="checkbox" checked={selectedTags.includes(tag)} onChange={() => onTagChange(tag)} />
-                        <span>{tag}</span>
+                        <input
+                            type="checkbox"
+                            checked={selectedTags.includes(tag)}
+                            onChange={() => onTagChange(tag)}
+                            className="mr2"
+                        />
+                        <span className="text-gray-700">{tag}</span>
                     </label>
                 </div>
             ))}
