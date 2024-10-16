b8ff6878163ec2a04262098507cc034e5f965d29
Jo <johj703@nate.com>
2024-10-16 17:19:11 +0900

    style: 게시글을 보여주는 컴포넌트에 CSS 추가

--- src/app/post/_components/PostList.tsx
+++ src/app/post/_components/PostList.tsx
@@ -14,12 +14,14 @@ const PostList = ({ posts }: PostListProps) => {
     return (
         <div>
             {posts.map((post) => (
-                <div key={post.id}>
-                    <div>{post.title}</div>
-                    <div>
+                <div key={post.id} className="mb-6 border-b pb-4 last:border-none">
+                    <div className="text-xl font-bold text-gray-800 mb-2">{post.title}</div>
+                    <div className="text-sm text-gray-600 space-x-2">
                         <span>{post.author}</span> <span>{post.date}</span>
                         {post.hashtags.slice(0, 3).map((tag, idx) => (
-                            <span key={idx}>{tag}</span>
+                            <span key={idx} className="text-blue-600">
+                                {tag}
+                            </span>
                         ))}
                     </div>
                 </div>
